9ae61a0534d99a34b22a2b5609f9efdebfa27c14
Gurpreet Singh <gpreetbhangu92@gmail.com>
2021-08-17 23:59:22 +0530

    Cleanups

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
-
 import './App.css';
+import { Button, BUTTON_COLOR } from './atoms';
 
 function App() {
   return (
@@ -11,35 +11,21 @@ function App() {
       plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
       initialView="timeGridFiveDay"
       weekends={false}
+      dayHeaders={true}
       events={[
+        { title: 'Hotel One', date: '2021-08-17', allDay: true, color: 'transprent' },
+        { title: 'Hotel Two', date: '2021-08-18', allDay: true, color: 'blue' },
+        { title: 'Hotel Three', date: '2021-08-19', allDay: true, color: 'blue' },
+        { title: 'Hotel Four', date: '2021-08-20', allDay: true, color: 'blue' },
         {
           title: 'Birmingham Comic Con',
           start: new Date('2021-08-17T09:00'),
-          end: new Date('2021-08-17T19:00'),
-          id: '1',
-          isMultipleDay: true,
-          multipleDayEvents: [
-            {
-              start: new Date('2014-11-12T09:00'),
-              end: new Date('2014-11-15T19:00'),
-              description: 'Day 1',
-            },
-            {
-              start: new Date('2014-11-16T09:00'),
-              end: new Date('2014-11-18T19:00'),
-              description: 'Day 2',
-            },
-            {
-              start: new Date('2014-11-02T09:00'),
-              end: new Date('2014-11-05T19:00'),
-              description: 'Day 3',
-            },
-          ],
+          end: new Date('2021-08-17T10:00'),
         },
-        { title: 'event 1', date: '2021-08-17', time: '08:00', isTime: true },
-        { title: 'event 2', date: '2021-08-18', time: '08:00', isTime: true },
-        { title: 'event 3', date: '2021-08-19', time: '09:00' },
-        { title: 'event 4', date: '2021-08-20', time: '10:00' },
+        { title: 'event 1', start: new Date('2021-08-17T06:00'), end: new Date('2021-08-17T8:00'), isTime: true },
+        { title: 'event 2', start: new Date('2021-08-18T09:00'), end: new Date('2021-08-18T11:00'), isTime: true },
+        { title: 'event 3', start: new Date('2021-08-19T10:00'), end: new Date('2021-08-19T12:00') },
+        { title: 'event 4', start: new Date('2021-08-20T11:00'), end: new Date('2021-08-20T16:00') },
       ]}
       headerToolbar={{
         left: 'prev,next today',
@@ -61,15 +47,21 @@ function App() {
 }
 
 function renderEventContent(eventInfo: any) {
-  if (eventInfo.event.isTime) {
-    return <div>TIME {eventInfo.event.time}</div>;
+  if (eventInfo.event.allDay) {
+    return (
+      <div style={{ height: 120, width: '100%', display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+        <div>{eventInfo.event.date}</div>
+        <Button color={BUTTON_COLOR.PRIMARY} type="contained">
+          {eventInfo.event.title}
+        </Button>
+      </div>
+    );
   }
 
   return (
-    <div className=" bg-red-700">
-      <p>Hello this is testing </p>
-      <b>{eventInfo.timeText}</b>
+    <div style={{ backgroundColor: eventInfo.color, height: 120, width: 145 }}>
       <i>{eventInfo.event.title}</i>
+      <b>{eventInfo.timeText}</b>
       <button onClick={() => eventInfo.eventClick()}>Close</button>
     </div>
   );
