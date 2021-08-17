a3b0ef6e4507e10ba6fe76eb752fa79548c05ed0
Gurpreet Singh <gpreetbhangu92@gmail.com>
2021-08-17 22:36:19 +0530

    Events

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
 import './App.css';
 
 function App() {
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]}
+      plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
       initialView="dayGridMonth"
-      weekends={false}
+      weekends={true}
       events={[
-        { title: 'event 1', date: '2021-08-17' },
-        { title: 'event 2', date: '2021-08-18' },
+        { title: 'event 1', date: '2021-08-17', time: '08:00', isTime: true },
+        { title: 'event 2', date: '2021-08-18', time: '08:00', isTime: true },
+        { title: 'event 3', date: '2021-08-19', time: '09:00' },
+        { title: 'event 4', date: '2021-08-20', time: '10:00' },
       ]}
+      headerToolbar={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+      }}
+      select={() => alert('Event Selected')}
       eventContent={renderEventContent}
+      eventClick={() => alert('You have clicked on event')}
     />
   );
 }
 
 function renderEventContent(eventInfo: any) {
+  if (eventInfo.event.isTime) {
+    return <div>TIME {eventInfo.event.time}</div>;
+  }
+
   return (
-    <div className="bg-gray-700">
+    <div className=" bg-red-700">
       <p>Hello this is testing </p>
       <b>{eventInfo.timeText}</b>
       <i>{eventInfo.event.title}</i>
-      <button>Close</button>
+      <button onClick={() => eventInfo.eventClick()}>Close</button>
     </div>
   );
 }
