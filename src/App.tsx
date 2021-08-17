b57005112e4cfa70266b1b21a70a6812a6c207a3
Gurpreet Singh <gpreetbhangu92@gmail.com>
2021-08-17 22:25:12 +0530

    Full https://github.com/fullcalendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import './App.css';
-import { Button } from './atoms';
 
 function App() {
   return (
-    <div className="flex flex-column space-x-2">
-      <Button title="Text Button" />
-      <Button title="Outlined Button" type="outlined" />
-      <Button title="Contained Button" type="contained" />
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      weekends={false}
+      events={[
+        { title: 'event 1', date: '2021-08-17' },
+        { title: 'event 2', date: '2021-08-18' },
+      ]}
+      eventContent={renderEventContent}
+    />
+  );
+}
+
+function renderEventContent(eventInfo: any) {
+  return (
+    <div className="bg-gray-700">
+      <p>Hello this is testing </p>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+      <button>Close</button>
     </div>
   );
 }
